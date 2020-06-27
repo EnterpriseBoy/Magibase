1ddcd58589c2b20221f0b20135e2a11ff18a501b
Niall Maguire <niall.maguire@topmail.ie>
2020-06-27 22:44:16 +0100

    change made

--- MagiApi/Controllers/WeatherForecastController.cs
+++ MagiApi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace MagiApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Maddi Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
