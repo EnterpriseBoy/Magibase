502684c91d42a95f401d8b14ecf0671f8593da4c
Niall Maguire <niall.maguire@topmail.ie>
2020-06-29 21:52:39 +0100

    adding events controller

---
+++ MagiApi/Controllers/EventsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MagiApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventsController : ControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetEvents()
+        {
+            return new JsonResult(new List<object>()
+            {
+                new {id=1,Name="Event One"},
+                new {id=2,Name="Event Two"}
+            });
+        }
+    }
+}
